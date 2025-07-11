30ebe1054df6c3c495df2fc44c6ea3587f58b9ec
akhil <akhilreddy723@gmail.com>
2025-07-11 11:15:33 +0530

    declared functionalities

---
+++ src/main/java/com/chronos_couriers_service/DispatchCentre.java
@@ -0,0 +1,22 @@
+package com.chronos_couriers_service;
+
+public class DispatchCentre {
+    public void placeOrder(){
+
+    }
+    public void updateRiderStatus(){
+
+    }
+    public void assignPackageToRider(){
+
+    }
+    public void processNewPackages(){
+
+    }
+    public void deliveryCompletion(){
+
+    }
+    public void getStatus(){
+
+    }
+}
